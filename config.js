7151620a52065b7993b126f495639f2d62fb372c
Raashul <rashul1996@gmail.com>
2018-03-19 20:41:55 -0400

    config

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  consumer_key:  '9N2blByGH9BJ8JTXDfXtTREOf',
+  consumer_secret:      'rl9tHQhVc3IX24Qolj5mdaUa4RzHdlVmUajQP5rGyMuzrSaSiz',
+  access_token:         '971599988496224256-dpPtSepvcg2a6NK59e0jirnypdNQ8TV',
+  access_token_secret:  'yOyodKfBvsgErLB1RZXWUwOe7CimEssdWQBEX3lQgahzU',
+  mongoURI: 'mongodb://Rashul:Password12@ds111059.mlab.com:11059/infourminutes-twitter-bot'
+}
