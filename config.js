3ae5d9c6469db8587aa92115a6c2a37f29a3e33b
Rashul Rajbhandari <rashul1996@gmail.com>
2018-04-12 19:26:54 -0400

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  consumer_key:  'YISm8tXj7EhwNyr2YyIVHYUEZ',
-  consumer_secret:'TrrmSTzPZejvTuYncewzV350Uhtb7XvmnpQ4ElYCmoIMHproJ8',
-  access_token:  '982326144841146369-Suwv7jAMgWFubU20IKjdbj7uXvOw14V',
-  access_token_secret: 'iaxbCCBWNYdoG3Ke018BJ3YG2oWrPf2VNEa4VM3D4TRd5',
-  mongoURI: 'mongodb://Rashul:Password12@ds111059.mlab.com:11059/infourminutes-twitter-bot'
-}
