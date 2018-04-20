a16324f485e147911fe42e0a040a4d63f5ecab69
Raashul <rashul1996@gmail.com>
2018-04-20 15:28:29 -0400

    reduce limit

--- info/info.js
+++ info/info.js
@@ -6,6 +6,6 @@ module.exports = {
       q: '#bitcoin OR #ethereum OR #dash OR #ripple OR #ipfs OR #ssb',
       result_type: 'recent',
       lang: 'en',
-      count: 50, //put 100,
+      count: 30, //put 100,
     }
 }
